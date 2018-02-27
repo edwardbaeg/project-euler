5cce37da4b43dfea2f8dc1cb964a5e8c2152bc83
edwardbaeg <edwardbaeg9@gmail.com>
2018-02-26 20:45:20 -0800

    Add 011-014.js

---
+++ javascript/014.js
@@ -0,0 +1,44 @@
+/*
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
+
+var longestCollatzSequence = function() {
+  var distances = [0,1];
+  var longest = 0;
+  var longestNum = 0;
+  for (let i = 1; i <= 1000000; i++) {
+    populateDistances(i);
+    if (distances[i] > longest) {
+      longest = distances[i];
+      longestNum = i;
+    }
+  }
+
+  function populateDistances(num) {
+    if(distances[num] !== undefined) {
+      return distances[num];
+    }
+    if (num % 2 === 0) {
+      distances[num] = populateDistances(num/2) + 1;
+      return distances[num];
+    } else{
+      distances[num] = populateDistances(num*3+1) + 1;
+      return distances[num];
+    }
+  }
+  return longestNum;
+};
+
+ console.log(longestCollatzSequence())
